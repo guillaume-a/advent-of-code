e82b9d55be6cf63500f350fd76b1cd82042e1b0e
Guillaume Aveline <guillaume@codr.fr>
2021-12-03 09:37:09 +0100

    [day 1] part 2

--- day-01-2.php
+++ day-01-2.php
@@ -2,10 +2,29 @@
 
 $lines = explode(PHP_EOL, file_get_contents(__DIR__ . '/inputs/input-01-1.txt'));
 
+//Calculate sums
+$sums = [];
+
+foreach($lines as $index => $line) {
+  if(!array_key_exists($index, $sums)) {
+    $sums[$index] = 0;
+  }
+
+  $sums[$index] += (int) $line;
+
+  if($index >= 1) {
+    $sums[$index-1] += (int) $line;
+  }
+  if($index >= 2) {
+    $sums[$index-2] += (int) $line;
+  }
+}
+
+//calculate increased count
 $increased = 0;
 $previous = PHP_INT_MAX;
 
-foreach($lines as $line) {
+foreach($sums as $line) {
   if($line > $previous) {
     $increased++;
   }
