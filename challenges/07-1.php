5883125b8fb8f821876d5a3d6e9d800f61141c5b
Guillaume Aveline <guillaume@codr.fr>
2021-12-07 09:13:34 +0100

    [day 7] part 1

---
+++ challenges/07-1.php
@@ -0,0 +1,23 @@
+<?php
+
+$values = explode(',', reset($lines));
+
+// How to Calculate Percentile
+// https://www.wikiwand.com/en/Percentile
+
+// The nearest-rank method
+// Sort dataset
+sort($values);
+
+//Calculate the rank r for the percentile p
+$n = count($values);
+$p = 50;
+$r = ($p / 100) * ($n - 1) + 1;
+
+$percentile = $values[floor($r)];
+
+$fuel = array_reduce($values, function($fuel, $position) use ($percentile) {
+  return $fuel + abs($position - $percentile);
+}, 0);
+
+var_dump($fuel);
