f9e13dbe0ee22731fd76f0692d29d2cd7eb07ec7
Guillaume Aveline <guillaume@codr.fr>
2021-12-08 20:31:07 +0100

    [day 08] part 2

--- src/Challenges/Year_2021/Day_08/Challenge.php
+++ src/Challenges/Year_2021/Day_08/Challenge.php
@@ -22,7 +22,7 @@ b    .  b    .  .    c  b    c  b    c
  dddd    dddd    ....    dddd    dddd
 .    f  e    f  .    f  e    f  .    f
 .    f  e    f  .    f  e    f  .    f
-gggg    gggg    ....    gggg    gggg
+ gggg    gggg    ....    gggg    gggg
 
  */
 class Challenge extends ChallengeBase {
@@ -32,7 +32,7 @@ class Challenge extends ChallengeBase {
     $anwser = 0;
 
     foreach($this->lines as $entry) {
-      list($in, $out) = explode(' | ', $entry);
+      [$in, $out] = explode(' | ', $entry);
       $digits = explode(' ', $out);
 
       $anwser += count(array_filter($digits, function($digit) {
@@ -45,6 +45,105 @@ class Challenge extends ChallengeBase {
   }
 
   public function part2(): string {
-    return '';
+
+    $anwser = 0;
+
+    foreach($this->lines as $entry) {
+      [$in, $out] = explode(' | ', $entry);
+
+      // Guess each number
+      $inDigits = explode(' ', $in);
+
+      // Easy ones
+      $one = $this->findOneByLength($inDigits, 2);
+      $four = $this->findOneByLength($inDigits, 4);
+      $seven = $this->findOneByLength($inDigits, 3);
+      $height = $this->findOneByLength($inDigits, 7);
+
+      // Find Two, Three and Five
+      $fiveDigits = $this->findByLength($inDigits, 5);
+
+      foreach($fiveDigits as $fiveDigit) {
+
+        //var_dump($fiveDigit);
+
+        $tmp = $this->diff($fiveDigit, $one);
+
+        if(strlen($tmp) === 3) {
+          $three = $fiveDigit;
+        } else {
+          $tmp = $this->diff($tmp, $four);
+
+          if(strlen($tmp) === 2) {
+            $five = $fiveDigit;
+          } else {
+            $two = $fiveDigit;
+          }
+        }
+      }
+
+      // Six, Nine and Zero
+      $sixDigits = $this->findByLength($inDigits, 6);
+
+      foreach($sixDigits as $sixDigit) {
+        $tmp = $this->diff($sixDigit, $one);
+
+        if(strlen($tmp) === 5) {
+          $six = $sixDigit;
+        } else {
+          $tmp = $this->diff($tmp, $four);
+
+          if(strlen($tmp) === 2) {
+            $nine = $sixDigit;
+          } else {
+            $zero = $sixDigit;
+          }
+        }
+      }
+
+      // Arrange digit in array
+      $numbers = [
+        $this->sort($zero) => '0',
+        $this->sort($one) => '1',
+        $this->sort($two) => '2',
+        $this->sort($three) => '3',
+        $this->sort($four) => '4',
+        $this->sort($five) => '5',
+        $this->sort($six) => '6',
+        $this->sort($seven) => '7',
+        $this->sort($height) => '8',
+        $this->sort($nine) => '9',
+      ];
+
+      // Calculate output
+      $outDigits = explode(' ', $out);
+      $number = '';
+
+      foreach ($outDigits as $outDigit) {
+        $number .= $numbers[$this->sort($outDigit)];
+      }
+
+      $anwser += (int) $number;
+    }
+
+    return $anwser;
+  }
+
+  private function findByLength($digits, $length): array {
+    return array_values(array_filter($digits, function($d) use ($length) { return strlen($d) === $length; }));
+  }
+
+  private function findOneByLength($digits, $length): string {
+    return $this->findByLength($digits, $length)[0];
+  }
+
+  private function diff($str1, $str2) {
+    return implode('', array_diff(str_split($str1), str_split($str2)));
+  }
+
+  private function sort($str) {
+    $a = str_split($str);
+    sort($a);
+    return implode('', $a);
   }
 }
