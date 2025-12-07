ce41343a8fd530632d48c5e3599e4d9272883938
Guillaume Aveline <guillaume@codr.fr>
2025-12-07 19:22:25 +0100

    2025/06 part 2

--- src/Challenges/Year2025/Day06.php
+++ src/Challenges/Year2025/Day06.php
@@ -54,7 +54,72 @@ class Day06 extends ChallengeBase
 
     public function partTwo(): string
     {
-        // TODO: Implement partTwo
-        return '';
+        $grandTotal = 0;
+        $lineCount = 0;
+        $colCount = 0;
+
+        $rows = [];
+
+        foreach ($this->lines as $line) {
+            $row = str_split($line);
+
+            // On garde cette fois la taille de ligne la plus grande.
+            $lineSize = \count($row);
+
+            if ($lineSize > $colCount) {
+                $colCount = $lineSize;
+            }
+
+            ++$lineCount;
+
+            $rows[] = $row;
+        }
+
+        // --$lineCount;
+
+        $isOperand = false;
+        $strNumbers = '';
+        $operand = '';
+
+        while ($colCount > 0) {
+            for ($j = 0; $j < $lineCount; ++$j) {
+                $chr = $rows[$j][$colCount - 1] ?? '';
+
+                if ('+' === $chr || '*' === $chr) {
+                    $isOperand = true;
+                    $operand = $chr;
+                } elseif (preg_match('/[0-9]/', $chr)) {
+                    $strNumbers .= $chr;
+                }
+            }
+
+            $strNumbers .= ' ';
+
+            if ($isOperand) {
+                $strNumbers = trim($strNumbers);
+                // echo 'do stuff with : ';
+                // echo '['.$strNumbers.']'.\PHP_EOL;
+
+                $littleTotal = '*' === $operand ? 1 : 0;
+
+                $arrayNumbers = explode(' ', $strNumbers);
+
+                foreach ($arrayNumbers as $number) {
+                    $littleTotal = match ($operand) {
+                        '+' => $littleTotal + (int) $number,
+                        '*' => $littleTotal * (int) $number,
+                        default => throw new \Exception('Unknown operand: '.$operand),
+                    };
+                }
+
+                $grandTotal += $littleTotal;
+                $strNumbers = '';
+                $isOperand = false;
+            }
+
+            --$colCount;
+        }
+
+        return (string) $grandTotal;
     }
 }
