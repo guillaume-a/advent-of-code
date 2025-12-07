213eba2e681e97020f3318b8c5b2fddd4b6a319f
Guillaume Aveline <guillaume@codr.fr>
2025-12-07 18:45:29 +0100

    2025/05 part 2 (not done)

--- src/Challenges/Year2025/Day05.php
+++ src/Challenges/Year2025/Day05.php
@@ -8,7 +8,9 @@ use Joky\AdventOfCode\Challenges\ChallengeBase;
 
 class Day05 extends ChallengeBase
 {
+    /** @var array<int, array{int, int}> */
     private array $ranges = [];
+    /** @var array<int, int> */
     private array $ids = [];
 
     public function partOne(): string
@@ -35,12 +37,78 @@ class Day05 extends ChallengeBase
 
     public function partTwo(): string
     {
+        $this->splitData();
+
+        $mergedRanges = [];
         $count = 0;
+
+        foreach ($this->ranges as $index => &$range) {
+            // $mergedRanges[] = $index;
+            if (\array_key_exists($index, $mergedRanges)) {
+                // echo $index.' skip merged'.PHP_EOL;
+                continue;
+            }
+
+            foreach ($this->ranges as $otherIndex => $otherRange) {
+                if ($index === $otherIndex) {
+                    // echo 'skip myself'.PHP_EOL;
+                    continue;
+                }
+
+                if (\array_key_exists($otherIndex, $mergedRanges)) {
+                    // echo $index.' skip merged'.PHP_EOL;
+                    continue;
+                }
+
+                // if ($index > $otherIndex) {
+                // echo 'skip myself'.PHP_EOL;
+                // continue;
+                // }
+
+                // L'autre range est hors de notre range, out of bounds, on passe a l'autre range suivant
+                if ($range[1] < $otherRange[0] || $range[0] > $otherRange[1]) {
+                    // echo 'skip oob'.PHP_EOL;
+                    continue;
+                }
+
+                // Saved other as merged
+                $mergedRanges[] = $otherIndex;
+
+                // echo 'merge'.PHP_EOL;
+                echo 'merge '.$index.' with '.$otherIndex.\PHP_EOL;
+                echo $range[0].' - '.$range[1].\PHP_EOL;
+                echo $otherRange[0].' - '.$otherRange[1].\PHP_EOL;
+
+                if ($otherRange[0] < $range[0]) {
+                    $range[0] = $otherRange[0];
+                }
+
+                if ($otherRange[1] > $range[1]) {
+                    $range[1] = $otherRange[1];
+                }
+
+                echo $range[0].' - '.$range[1].\PHP_EOL.\PHP_EOL;
+            }
+
+            // Saved self as merged
+            // $mergedRanges[] = $index;
+        }
+
+        foreach ($mergedRanges as $index) {
+            unset($this->ranges[$index]);
+        }
+
+        foreach ($this->ranges as $mergedRange) {
+            $count += ($mergedRange[1] - $mergedRange[0] + 1);
+        }
+
+        echo \count($this->ranges).\PHP_EOL;
+        // var_dump($this->ranges);
 
         return (string) $count;
     }
 
-    private function splitData()
+    private function splitData(): void
     {
         $ids = false;
         foreach ($this->lines as $line) {
@@ -49,7 +117,8 @@ class Day05 extends ChallengeBase
                 continue;
             }
             if (!$ids) {
-                $this->ranges[] = array_map(fn ($v) => (int) $v, explode('-', $line));
+                $parts = array_map(fn ($v) => (int) $v, explode('-', $line));
+                $this->ranges[] = [$parts[0], $parts[1]];
                 continue;
             }
 
